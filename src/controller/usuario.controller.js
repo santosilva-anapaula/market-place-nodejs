ab7036b881513c7182a5e8aa5d2fcb25c93534b4
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-09 16:42:49 -0300

    Feat: Rotas de usuario e controller

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,92 @@
+const findUserByIdController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const findAllUsersController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const createUserController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const updateUserController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const removeUserController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const addUserAddressController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const removeUserAddressController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const addUserFavProductController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+const removeUserFavProductController = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(`erro:${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado tente novamente!`});
+    }
+};
+
+module.exports = {
+    findUserByIdController,
+    findAllUsersController,
+    createUserController,
+    updateUserController,
+    removeUserController,
+    addUserAddressController,
+    removeUserAddressController,
+    addUserFavProductController,
+    removeUserFavProductController
+}
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -4,17 +4,17 @@ const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
 //Configurações dos endpoints
-router.get('/findById:id');
-router.get('findAll');
+router.get('/findById:id', usuarioController.findUserByIdController);
+router.get('findAll', usuarioController.findAllUsersController);
 
-router.post('/create');
-router.post('/addAddress/:id');
-router.post('/addFavProduct/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('/addAddress/:id', usuarioController.addUserAddressController);
+router.post('/addFavProduct/:id', usuarioController.addUserFavProductController);
 
-router.put('/update/:id');
+router.put('/update/:id', usuarioController.updateUserController);
 
-router.delete('/remove/:id');
-router.delete('/removeAddress/');
-router.delete('/removeFavProduct');
+router.delete('/remove/:id', usuarioController.removeUserController);
+router.delete('/removeAddress/', usuarioController.removeUserAddressController);
+router.delete('/removeFavProduct', usuarioController.removeUserFavProductController);
 
 module.exports = router;
