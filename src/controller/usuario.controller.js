d8bbbb55015f2ea4f84a692b5a90d010aeca26c3
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-09 19:20:29 -0300

    Feat: User controller atualizado

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,7 +1,21 @@
+const userService = require("../service/usuario.service");
+
 const findUserByIdController = async (req, res) => {
-    try {
+    try {        
+        const user = await userService.findUserByIdService(req.params.id);
+        
+        if(!user){
+            return res. status(404).send({ message: "Usuário não encontrado tente novamente"});
+        }
+
+        return res.status(200).send(user);
         
     } catch (err) {
+        if (err.kind == "ObjectId") {
+            console.log(err.kind == "ObjectId");
+            return res.status(400).send({ message: `ID informado, está incorreto. Tente novamente!`});
+        }
+
         console.log(`erro:${err.message}`);
         return res.status(500).send({ message: `Erro inesperado tente novamente!`});
     }
@@ -9,7 +23,8 @@ const findUserByIdController = async (req, res) => {
 
 const findAllUsersController = async (req, res) => {
     try {
-        
+        return res.status(200).send(await userService.findAllUserService());
+
     } catch (err) {
         console.log(`erro:${err.message}`);
         return res.status(500).send({ message: `Erro inesperado tente novamente!`});
@@ -18,7 +33,13 @@ const findAllUsersController = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try {
-        
+        const body = req.body;
+        if(!body.nome){
+            return res.status(400).send({ message: `O campo 'nome' precisa ser preenchhido`});
+        }
+
+        return res.status(201).send(await userService.createUserService(body));
+
     } catch (err) {
         console.log(`erro:${err.message}`);
         return res.status(500).send({ message: `Erro inesperado tente novamente!`});
@@ -27,6 +48,12 @@ const createUserController = async (req, res) => {
 
 const updateUserController = async (req, res) => {
     try {
+        const body = req.body;
+        if(!body.nome){
+            return res.status(400).send({ message: `O campo 'nome' precisa ser preenchhido`});
+        }
+
+        return res.send(await userService.updateUserService(req.params.id, body));
         
     } catch (err) {
         console.log(`erro:${err.message}`);
@@ -36,6 +63,13 @@ const updateUserController = async (req, res) => {
 
 const removeUserController = async (req, res) => {
     try {
+        const deletedUser = await userService.removeUserService(req.params.id);
+
+        if (deletedUser.deletedCount > 0){
+            res.status(200).send({ message: `Sucesso usuario deletado!`});
+        }else{
+            res.status(400).send({ message: `Usuario nao encontrado, tente novamente!`});
+        }
         
     } catch (err) {
         console.log(`erro:${err.message}`);
