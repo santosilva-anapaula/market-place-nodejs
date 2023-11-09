efbc64c4ef11ae67fa0169e3f5c9a90b8f3e1008
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-09 16:16:43 -0300

    Feat: Scripts, rotas de usuario e controller

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express"); //chamando o express
 const connectToDatabase = require("./src/database/database"); //importação do banco de dados
 
+const usuario = require("./src/router/usuario.router");
+
 const app = express(); //defini que o app é o express
 
 const port = 3000; //define a porta da aplicação
 
 app.use(express.json()); //trabalhar o conteudo da aplicação com json
 
+
 connectToDatabase();
+
+app.use("/usuario", usuario);
 
 app.get("/", (req, res) => {
     res.send({
