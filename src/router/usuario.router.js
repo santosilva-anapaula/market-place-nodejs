45246132df70ca1dba2a26d7379e6b456a6238cd
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-13 11:56:33 -0300

    Fix: correção de rota do usuario para findById

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
 
 //rotas get
-router.get('/findById:id', usuarioController.findUserByIdController);
+router.get('/findById/:id', usuarioController.findUserByIdController);
 router.get('/findAll', usuarioController.findAllUsersController);
 
 //rotas post
