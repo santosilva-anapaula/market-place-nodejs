43586e4ab0c2ce2bca8bf5da4d0b84f2cd3b5afd
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-13 12:54:44 -0300

    Fix: endereco do banco + remove prod fav + user

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose"); //chamando o mongoose
 
 function connectToDatabase() {
-    mongoose.connect("mongodb://127.0.0.1:27017",{
+    mongoose.connect("mongodb://127.0.0.1:27017/market-place",{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -15,12 +15,12 @@ const UsuarioSchema = new mongoose.Schema({
         }
     ],
     createdAt: { type: Date, required: true, default: Date.now() },
-    produtos_fav: [
-        {
-            _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos" },
-            createdAt: { type: Date, required: true, default: Date.now() },
-        }
-    ],
+    // produtos_fav: [
+    //     {
+    //         _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos" },
+    //         createdAt: { type: Date, required: true, default: Date.now() },
+    //     }
+    // ],
     admin: { type: Boolean, required: true, default: false }
 }); //construção do modelo de dados
 
