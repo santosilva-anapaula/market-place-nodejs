32f36481f809eb4811049724d636fcdcb79ecf3f
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-17 13:50:19 -0300

    Fix: corrige tipo de dado em carrinho.js

--- src/model/Carrinho.js
+++ src/model/Carrinho.js
@@ -7,7 +7,7 @@ const CarrinhoSchema = new mongoose.Schema({
             quantidade: { type: Number, required: true }
         },
     ],
-    createAt: { type: Date(), required: true, default: Date.now },
+    createAt: { type: Date, required: true, default: Date.now },
     precoTotal: { type: Number, required: true },
     frete: { type: Number, required: true },
     userId:  { type: mongoose.Schema.Types.ObjectId, ref: "usuarios" }
