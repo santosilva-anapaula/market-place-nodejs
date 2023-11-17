ea85f7ea95e29c8ac67af13c21aa03c43b46c28d
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-11-17 20:15:59 -0300

    Feat: valida login implementado

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const authController = require("../controller/auth.controller");
+const{ validaLogin } = require("../middleware/validacao.middleware");
 
 // rota de login
-router.post("/login", authController.loginController);
+router.post("/login", validaLogin, authController.loginController);
 
 module.exports = router;
